5743bca78bd9611e311bc8ede5bc5c6c9f12973b
TJ Cheetham <cheetham.tj@gmail.com>
2019-10-23 22:52:17 -0700

    added current date

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-$(this).siblings(".text").val()
+// $(this).siblings(".text").val()
+
+// var letterBtn = $("<button>");
+
+// var date = new Date();
+// $(".date").innerHTML = date;
+
+var date = new Date();
+document.getElementById("date").innerHTML = date;
+
+// var entryInput = document.$("#text");
+
+$(".save").on("click", function () {
+    console.log("clicked");
+   var entryInput = $(this).siblings(".text").val()
+    localStorage.setItem("entryInput", input.val());
+});
